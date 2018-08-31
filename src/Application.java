e9b8988aabeeed97fec6af8e91a8733471e40e10
SON NGUYEN <myantj3m@gmail.com>
2018-08-31 11:29:39 +0700

    find the largest common divisor

---
+++ src/Application.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = sc.nextInt();
+        System.out.println("Enter b: ");
+        b = sc.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0){
+            System.out.println("No greatest common factor");
+            return;
+        }
+
+        while (a != b) {
+            if (a > b)
+                a -= b;
+            else
+                b -= a;
+        }
+        System.out.println("Greatest common factor: " + a);
+        return;
+    }
+}
